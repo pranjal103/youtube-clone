a5da5ed92e44e079e90606d0df41ab692d2b4369
pranjal103 <pranjaltogalekar@gmail.com>
2025-02-15 17:04:35 +0530

    Initial commit - set up project structure

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Import Authentication Routes (set up in Phase 4)
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB using the connection string in the .env file
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error(err));
 
+// Default route to test the API
 app.get('/', (req, res) => {
   res.send('Welcome to YouTube Clone API');
 });
