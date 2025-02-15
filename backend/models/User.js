5136f57e66b719861873b9cef3318d117b5195d8
pranjal103 <pranjaltogalekar@gmail.com>
2025-02-15 17:08:07 +0530

    Your commit message

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  avatar: { type: String }
-});
-module.exports = mongoose.model('User', UserSchema);
 
+const UserSchema = new mongoose.Schema({
+  username: { 
+    type: String,
+    required: true
+  },
+  email: { 
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: { 
+    type: String,
+    required: true
+  },
+  avatar: { 
+    type: String
+  },
+  channels: [{ 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Channel'
+  }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
